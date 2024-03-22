5ae3796528e078d907850eaee8c8b68d2d4d5217
CKa3o4HuK <71247156+N3minator@users.noreply.github.com>
2024-03-22 22:32:13 +0100

    Update BookLibrary_ru.py

--- Exercise 11.03.24/BookLibrary_ru.py
+++ Exercise 11.03.24/BookLibrary_ru.py
@@ -73,6 +73,9 @@ class LoginRegisterApp:
         self.cursor = self.conn.cursor()
         self.create_tables()
 
+    def get_cursor(self):
+        return self.cursor
+
     def focus_next_widget(self, widget):
         """Перемещает фокус на следующий виджет."""
         widget.focus_set()
@@ -211,7 +214,10 @@ class LoginRegisterApp:
 
     # Реализуем функционал кнопки Пропустить Регистрацию
     def skip_register_user(self):
-        messagebox.showwarning('Осторожно!', 'Вы вошли как Гость!')
+        # Сохраняем логин пользователя перед закрытием окна
+        self.user_login = self.login_entry.get().strip()
+
+        #messagebox.showwarning('Осторожно!', 'Вы вошли как Гость!')
         self.open_library()
 
     # Реализуем функционал кнопки Сбросить Ввод
@@ -219,34 +225,244 @@ class LoginRegisterApp:
         self.login_entry.delete(0, tk.END)
         self.password_entry.delete(0, tk.END)
 
-    # Функция для открытия окна калькулятора
+    # Функция для открытия окна Библиотеки Книг
     def open_library(self):
+        # Сохраняем логин пользователя перед закрытием окна
+        self.user_login = self.login_entry.get().strip()
+
         # Закрываем окно входа/регистрации
         self.root.destroy()
-        # Функция для открытия окна калькулятора
-        calc_win = tk.Tk()
+        # Инициализируем новое окно Библиотеки Книг
+        win_booklibrary = tk.Tk()
 
-        # Передача окна калькулятора в класс BookLibrary
-        BookLibrary(calc_win)
+        # Передача окна Библиотеки Книг/курсора/логина в класс BookLibrary
+        BookLibrary(win_booklibrary, self.cursor,  self.conn, self.user_login)
 
         # Переименовываем программу
-        calc_win.title("Библиотека Книг")
+        win_booklibrary.title("Библиотека Книг")
 
         # Устанавливаем расширение по умолчанию
-        calc_win.geometry(f'238x265')
+        win_booklibrary.geometry(f'325x200')
         # Запрещаем менять размер окна
-        calc_win.resizable(width=False, height=False)
+        win_booklibrary.resizable(width=False, height=False)
         # Устанавливаем цвет фона приложения
-        calc_win['bg'] = 'black'
+        win_booklibrary['bg'] = 'black'
 
         # Режим ожидания
-        calc_win.mainloop()
+        win_booklibrary.mainloop()
 
 
 class BookLibrary (tk.Frame):
 
-    def __init__(self, root):
-        print('hi')
+    def __init__(self, win_library, cursor, connect, login):
+
+        super().__init__(win_library)
+        self.win_library = win_library
+        self.cursor = cursor
+        self.conn = connect
+        self.login = login
+
+        self.win_app_books = WinAppBooks()  # Объект класса WinAppBooks
+
+        # Создание кнопки для меню выбора
+        # Хочу потом сделать чтобы через цикл for выводился текст только для пользователей без админки, или если аккаунт под админкой то выводился текст дополнительно для Админов
+        self.choice_label = tk.Label(win_library, text='''Что вы хотите сделать?
+        1. Посмотреть доступные книги
+        2. Посмотреть взятые книги пользователями (Админ)
+        3. Взять на прокат
+        4. Вернуть книгу
+        5. Посмотреть ваш список взятых книг
+        6. Добавить книгу (Админ)
+        7. Удалить книгу (Админ)
+        0. Выйти''')
+        self.choice_label.grid(row=0, column=0)
+        self.choice_entry = tk.Entry(win_library)
+        self.choice_entry.grid(row=1, column=0, columnspan=2, padx=10, pady=5)
+
+        self.win_library.bind('<Return>', lambda event: self.choice())
+
+    def choice(self):
+        choice = self.choice_entry.get().strip()
+
+        if choice == '1':
+            # Открываем базу данных с доступными книгами
+            self.cursor.execute("SELECT * FROM books WHERE visible=1")
+            books = self.cursor.fetchall()
+
+            if books:
+                # Собираем информацию о книгах в одну строку
+                books_info = '\n'.join(
+                    [f"Номер книги: {book[0]} | Название книги: {book[1]} | Автор: {book[2]} | Жанр: {book[3]}\n" for book in books])
+                messagebox.showinfo('Доступные книги', books_info)
+            else:
+                messagebox.showinfo('Доступные книги', 'В данный момент нет доступных книг.')
+        elif choice == '2':
+            # Ищем пользователя в базе данных
+            self.cursor.execute("SELECT * FROM users WHERE login=?", (self.login,))
+            state_user = self.cursor.fetchone()
+
+            # Если результат не равен None, это означает, что пользователь найден (То что он не под Гостевым аккаунтом)
+            if state_user is not None:
+                # Открываем базу данных и ищем пользователя с таким логином и узнаем его статус Админа
+                self.cursor.execute("SELECT admin_state FROM users WHERE login=?", (self.login,))
+                state_admin = self.cursor.fetchone()
+
+                if state_admin[0] == 'True':
+                    self.list_rented_books()
+                else:
+                    messagebox.showerror('Отказ доступа!', 'Отказано в доступе! Вы не Админ!')
+            else:
+                messagebox.showerror('Ошибка!', 'Вы под Гостевым аккаунтом!')
+        elif choice == '3':
+            self.win_app_books.buy_books(self.cursor,  self.conn) # Вызываем метод buy_books() через объект WinAppBooks
+        elif choice == '4':
+            messagebox.showwarning('Недоступно!', 'На данный момент эта функция Разрабатывается!')
+        elif choice == '5':
+            messagebox.showwarning('Недоступно!', 'На данный момент эта функция Разрабатывается!')
+        elif choice in ['6', '7']:
+            # Ищем пользователя в базе данных
+            self.cursor.execute("SELECT * FROM users WHERE login=?", (self.login,))
+            state_user = self.cursor.fetchone()
+
+            # Если результат не равен None, это означает, что пользователь найден (То что он не под Гостевым аккаунтом)
+            if state_user is not None:
+                self.cursor.execute("SELECT admin_state FROM users WHERE login=?", (self.login,))
+                # if cursor.fetchone()[0] == 'True || Проверяет является ли пользователей Админом
+                if self.cursor.fetchone()[0] == 'True':
+                    if choice == "6":
+                        self.win_app_books.add_book(self.cursor,  self.conn)  # Вызываем метод add_book() через объект WinAppBooks
+                    else:
+                        self.win_app_books.del_book()  # Вызываем метод add_book() через объект WinAppBooks
+                else:
+                    messagebox.showerror('Отказ доступа!', 'Отказано в доступе! Вы не Админ!')
+            else:
+                messagebox.showerror('Ошибка!', 'Вы под Гостевым аккаунтом!')
+        elif choice == '0':
+            # Закрываем окно Библиотеки Книг (Закрываем программу полностью)
+            self.win_library.destroy()
+        else:
+            messagebox.showerror('Ошибка!', 'Вы ввели не верное число!')
+
+    def list_rented_books(self):
+        """Отображает список взятых книг."""
+        self.cursor.execute("SELECT * FROM user_books")
+        books = self.cursor.fetchall()
+
+        if books:
+            # Собираем информацию о книгах в одну строку
+            books_info = '\n'.join(
+                [f"ID Книги: {books[0]} | Название: {books[1]} | Автор: {books[2]} | Жанр: {books[3]} | Взято: {books[4]} | До: {books[5]} | Пользователь: {books[6]} | ID пользователя: {books[7]}\n" for book
+                 in books])
+            messagebox.showinfo('Доступные книги', books_info)
+        else:
+            messagebox.showwarning('Нету книг на прокате!', 'В данный момент не кто не взял книги :(')
+
+
+class WinAppBooks:
+
+    def buy_books(self, cursor, connect):
+        self.cursor = cursor
+        self.conn = connect
+
+    def add_book(self, cursor, connect):
+        self.cursor = cursor
+        self.conn = connect
+        # Инициализируем новое окно Добавление Книги
+        win_add_book = tk.Tk()
+
+        # Переименовываем программу
+        win_add_book.title("Добавление Книги")
+
+        # Устанавливаем расширение по умолчанию
+        win_add_book.geometry(f'325x200')
+        # Запрещаем менять размер окна
+        win_add_book.resizable(width=False, height=False)
+        # Устанавливаем цвет фона приложения
+        win_add_book['bg'] = 'black'
+
+        self.name_book_label = tk.Label(win_add_book, text="Введите название книги:")
+        self.name_book_label.grid(row=0, column=0, padx=10, pady=5)
+        self.name_book_entry = tk.Entry(win_add_book)
+        self.name_book_entry.grid(row=0, column=1, padx=10, pady=5)
+
+        self.author_book_label = tk.Label(win_add_book, text="Введите автора книги:")
+        self.author_book_label.grid(row=1, column=0, padx=10, pady=5)
+        self.author_book_entry = tk.Entry(win_add_book)
+        self.author_book_entry.grid(row=1, column=1, padx=10, pady=5)
+
+        self.genre_book_label = tk.Label(win_add_book, text="Введите жанр книги:")
+        self.genre_book_label.grid(row=2, column=0, padx=10, pady=5)
+        self.genre_book_entry = tk.Entry(win_add_book)
+        self.genre_book_entry.grid(row=2, column=1, padx=10, pady=5)
+
+        self.visible_book_label = tk.Label(win_add_book, text='''Книга доступна для проката?
+        (1 - да, 0 - нет):''')
+        self.visible_book_label.grid(row=3, column=0, padx=10, pady=5)
+        self.visible_book_entry = tk.Entry(win_add_book)
+        self.visible_book_entry.grid(row=3, column=1, padx=10, pady=5)
+
+        # Создание кнопки для Подтверждения
+        self.confirmation_button = tk.Button(win_add_book, text="Подтверждения", command=lambda: self.confirmation_add(win_add_book))
+        self.confirmation_button.grid(row=4, column=0, columnspan=2, padx=10, pady=5, sticky="we")
+
+        # Режим ожидания
+        win_add_book.mainloop()
+
+    def confirmation_add(self, win):
+
+        self.win_add_book = win
+
+        # Получаем список существующих идентификаторов Книг
+        self.cursor.execute("SELECT id_books FROM books ORDER BY id_books ASC")
+        existing_ids = [id_[0] for id_ in self.cursor.fetchall()]
+
+        # Находим первый свободный ID
+        new_id = 1
+        for id_ in existing_ids:
+            if id_ != new_id:
+                break
+            new_id += 1
+
+        name_book = self.name_book_entry.get()
+        author_book = self.author_book_entry.get()
+        genre_book = self.genre_book_entry.get()
+        visible_book = self.visible_book_entry.get()
+
+        # Проверяем валидность данных
+        if not self.check_add_book(name_book, author_book, genre_book, visible_book):
+            return
+
+        self.cursor.execute("INSERT INTO books (id_books, name_books, author, genre, visible) VALUES (?, ?, ?, ?, ?)",
+                       (new_id, name_book, author_book, genre_book, visible_book))
+        self.conn.commit()
+
+        messagebox.showinfo('Успех!', 'Вы добавили новую книгу!')
+
+        # Закрываем окно Добавление Книг
+        self.win_add_book.destroy()
+
+    def check_add_book(self, name_book, author_book, genre_book, visible_book):
+
+        # Чтобы получилось например из "  Vitalii    Derkach  " в "Vitalii Derkach"
+        name_book = re.sub(r'\s+', ' ', name_book).strip()
+        author_book = re.sub(r'\s+', ' ', author_book).strip()
+        genre_book = re.sub(r'\s+', ' ', genre_book).strip()
+        visible_book = re.sub(r'\s+', ' ', visible_book).strip()
+
+        visible_book = int(visible_book)
+
+        if name_book and author_book and genre_book:
+            if visible_book == 1 or visible_book == 0 and visible_book is not None:
+                return True
+            else:
+                messagebox.showerror('Ошибка!', 'Не правильное значение поля видимости!')
+                return False
+        else:
+            messagebox.showerror('Ошибка!', 'Поля не могут быть пустыми!')
+            return False
+
+    def del_book(self):
+        pass
 
 
 def open_login_register():
